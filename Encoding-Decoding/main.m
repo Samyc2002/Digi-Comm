10ddd12e5418e66923fa2d95dc0472e070f5a095
Samyc2002 <2000200402@iitdh.ac.in>
2022-11-09 22:19:17 +0530

    Revert "Added convolution and huffman encoder/decoder"
    This reverts commit a6f4c31da2a74483d1dfc0ed0ae9848f814512a4.

--- Encoding-Decoding/main.m
+++ Encoding-Decoding/main.m
@@ -2,43 +2,24 @@ clc;
 clear all;
 close all;
 
-% Generator matrix for linear encoding/decoding
-G = [1 0 1 0 1; 
-     0 1 0 1 1];
-
+% Generator matrix
+G = [1 0 0 0 0 1 1;
+     0 1 0 0 1 0 1;
+     0 0 1 0 1 1 0;
+     0 0 0 1 1 1 1];
 % Information Bits
-info = [0 0 1 0 0 0];
+info = [0 0 0 0];
+% Decoding Strategy
+strategy = "std";
 
 % Encoding to n bits
-% Using Linear Encoder
-% encodedBits = linearEncoder(G, info);
-% Using Convolutional Encoder
-encodedBits = convolutionEncoder(string(info));
+encodedBits = linearEncoder(G, info);
+% Spoiling the first bit
+encodedBits(1) = 1 - encodedBits(1);
+% Decoding with the defined strategy
+[H, dmin, stdArr, S, decodedBits] = linearDecoder(G, encodedBits, strategy);
 
-% Spoiling three bit
-% encodedBits(1) = 1 - encodedBits(1);
-% encodedBits(3) = 1 - encodedBits(3);
-% encodedBits(4) = 1 - encodedBits(4);
-
-% Decoding Strategy
-% strategy = "ml";
-
-% Decoding with the defined strategy (linear decoder)
-% [H, dmin, stdArr, S, decodedBits] = linearDecoder(G, encodedBits, strategy);
-
-% Decoding (convolution decoder)
-% disp(encodedBits);
-decodedBits = convolutionDecoder(encodedBits);
-
-% Display the output
-
-% For linear encoder/decoder
 % disp(H);
 % disp(dmin);
-% disp(length(unique(stdArr)));
-% disp(bin2dec(stdArr));
 % disp(S);
-% disp(decodedBits);
-
-% For convolutional encoder/decoder
 disp(decodedBits);
